92cecc430810b0cea0bac6ca32e986c07962efdf
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-07-26 21:36:37 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -11,9 +11,26 @@ class Pessoas {
 
     calcularIMC() { //aqui não precisa de parametro por que já está na classe Pessoas
         return this.peso / (this.altura * this.altura); 
+    } 
+
+    classificarIMC() {
+        const imc = this.calcularIMC();
+
+        if (imc < 18.5) {
+            return('Abaixo do peso!');
+        } else if (imc >= 18.5 && imc < 25) {
+            return('Peso normal');
+        } else if (imc >= 25 && imc < 30) {
+            return('Acima do peso');
+        } else if (imc >= 30 && imc < 40) {
+            return('Obesidade');
+        } else {
+            return('Obesidade Grave!!');
+        }
     }
 }
 
 const jose = new Pessoas('jose', 70, 1.75);
 
-console.log(jose.calcularIMC());
+console.log('Peso de José: ' + jose.calcularIMC());
+console.log(jose.classificarIMC())
